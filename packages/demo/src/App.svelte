04e5198205dfa2bbab01bd291b2c38a486f0100f
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2023-09-13 12:36:49 +0300

    init

---
+++ packages/demo/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  // import '../../../dist/lib/my-library';
+  import "../../lib"
+</script>
+
+<main>
+
+    <my-component
+            background="orange"
+    ></my-component>
+
+</main>
+
+<style>
+  main {
+    width: 70vw;
+  }
+</style>
